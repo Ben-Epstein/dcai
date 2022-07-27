a0bf92f275558a7fd89f4229e6e6fd759febb596
Ben Epstein <ben.epstein97@gmail.com>
2022-07-27 14:42:02 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+REQUIREMENTS_FILE = "requirements.txt"
+
+with open(REQUIREMENTS_FILE, "r") as dependencies_file:
+    DEPENDENCIES = dependencies_file.readlines()
+
+setup(
+    name="dcai",
+    version="0.0.0",
+    install_requires=DEPENDENCIES,
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Ben Epstein",
+    author_email="ben.epstein97@gmail.com",
+    description="A helper package for the requirements of the Data Centric AI Book",
+    url="https://github.com/Ben-Epstein/dcai/"
+)
